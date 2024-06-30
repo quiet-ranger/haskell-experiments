143fe9ab40f279afb30eac51c7cca6d2382472ab
Juan Jimenez (JJ) <juan.jimenez@pobox.com>
2024-06-30 17:02:38 +0200

    Some functions one of which seems to have a problem

--- hello.hs
+++ hello.hs
@@ -1,4 +1,10 @@
+module Main (main) where  -- no needed in the interpreter as it is the default in a module file
+
+main :: IO() -- you can leave this out and the compiler will infer it
 main = do
     putStrLn "Hello, eveybody!"
     putStrLn ("Please look at my favourite numbers: " ++ show (filter odd [10..25]))
-    
+
+factorial :: (Integral a) => a -> as
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
